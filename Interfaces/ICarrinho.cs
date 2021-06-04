b1f7761550b303227b105f6c97c27c429cc206b6
Hugo da Silva Vitor <hugodasilvav0209@gmail.com>
2021-06-04 18:06:28 -0300

    Aula finalizada

---
+++ Interfaces/ICarrinho.cs
@@ -0,0 +1,15 @@
+using ExemploInterface.Classes;
+
+namespace ExemploInterface.Interfaces
+{
+    public interface ICarrinho
+    {
+         void Listar();
+
+         void Cadastrar(Produto produto);
+
+         void Alterar(int _codigo, Produto produto);
+
+         void Deletar(Produto produto);
+    }
+}
